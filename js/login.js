b06c3fbbd3a4f16eae152925ff2c0dc224fe3864
Rafael Lima dos Santos <rafinha1990lima@gmail.com>
2022-11-09 22:47:49 -0300

    arrumando a tela login

--- js/login.js
+++ js/login.js
@@ -1,10 +1,18 @@
-const input = document.querySelector('.login-input');
-const button = document.querySelector('.login-button');
+const input = document.querySelector('.login_input');
+const button = document.querySelector('.login_button');
+const form = document.querySelector('.login-form');
 
-const validateInput = (event) => {
-    if (EventTarget.value.length > 2) {
-        button.removeAttribute('disable');
-    }
-} 
+const validateInput = ({ target }) => {
+  if (event.target.value.length > 2) {
+    button.removeAttribute('disabled');
+    return;
+  }
+  button.setAttribute('disabled', '');
+}
+
+const handleSubmit = () => {
+
+}
 
 input.addEventListener('input', validateInput);
+form.addEventListener('submit', handleSubmit);
