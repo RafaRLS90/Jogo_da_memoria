872b97eba7c9718e30d804f0db4178dd77efe901
Rafael Lima dos Santos <rafinha1990lima@gmail.com>
2022-11-15 12:12:44 -0300

    acrescentando funçãpo para as cartas ficarem aleatórias

--- js/game.js
+++ js/game.js
@@ -35,7 +35,12 @@ const createCard = (character) => {
 }
 
 const loadGame = () => {
-    characters.forEach((character) => {
+    
+        const duplicateCharacters = [ ... characters, ... characters];
+    
+        const shuffledArray = duplicateCharacters.sort( () => Math.random() - 0.5 );
+
+        duplicateCharacters.forEach((character) => {
         
         const  card =createCard(character);
         grid.appendChild(card);
